1f465cdb239ef68b6c76164db65ff37184bf009e
JZStudent <joshzepeda.student@gmail.com>
2018-06-24 19:38:18 -0500

    updated API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,152 @@
-
-var APIkey = "4f3188b24b25b236bcf5a2cffd75cfe6";
-var locationQuery = $("#locationInput").val().trim();
-var QueryURL ="api.openweathermap.org/data/2.5/forecast?q=" + locationQuery + "&units=imperial&appid=" + APIkey;
+//https://developer.nps.gov/api/v1/parks?stateCode=MN&api_key=N31BSTd4vcXAUWTFUb3FPdW4zBX1Jw3gVc5Sisw1
 
 $(document).ready(function() {
-    $("#submit-button").on("click", function(event) {
+    $("#submitButton").on("click", function(event) {
         event.preventDefault();
-        $.ajax({
-            url: QueryURL,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(QueryURL);
-            console.log(response);   
+        
+    // This line of code will grab the input from the textbox
+       var where = $("#where").val().trim();
+        $('#buttonInput').val('');
+    // The movie from the textbox is then added to our array
+        console.log(where)
+    // Calling renderButtons which handles the processing of our movie array
+    getParksByState(where)
+   // googleMaps(where)
+    });
+})
 
-            $("#temp").text("Temperature: " + response.main.temp);
-            $("#wind").text("Wind Speed: " + response.wind.speed);
-            $("#humidity").text("Humidty: " + response.main.humidity);
+
+  //clears the gifs if there are some there
+
+  
+//this is the jquery ajax call
+function getParksByState(locationQuery){
+
     
-            console.log("Temperature: " + response.main.temp);
-            console.log("Wind Speed: " + response.wind.speed);
-            console.log("Humidty: " + response.main.humidity);
-        });
+    var parksBaseURL = "https://developer.nps.gov/api/v1/parks?";
+
+    var parksStateCode = "stateCode=";
+
+    var parksAPIKey = "&api_key=N31BSTd4vcXAUWTFUb3FPdW4zBX1Jw3gVc5Sisw1";
+
+    var parksAJAX = parksBaseURL + parksStateCode + locationQuery + parksAPIKey ;
+
+    console.log(parksAJAX);
+
+
+
+    $.ajax({
+    //takes the URL which is our queryURL
+    url: parksAJAX,
+    //magic method of GET (something something SERVER HTTP STUFF something something)
+    method: "GET"
+    })
+    //happens after the promise above is fullfilled
+    .then(function(response) {
+        
+         console.log(response)
+        for(i = 0; i < response.data.length; i ++){
+
+            var newDiv = $("<div>");
+
+            var parkNameP = $("<h3>");
+
+            var parkDescP = $("<p>");
+
+            parkNameP.html(response.data[i].fullName);
+
+            parkDescP.html(response.data[i].description);
+
+            newDiv.append(parkNameP);
+
+            newDiv.append(parkDescP);
+
+            newDiv.addClass("joePlaceHolder clickable")
+
+            newDiv.attr("latLong", response.data[i].latLong)
+
+            newDiv.attr("fullName", response.data[i].fullName)
+
+            $("#displayParks").append(newDiv)
+
+        }
+
+    });
+}
+
+// $('body').on('click', '.clickable', function () {
+
+//     var 
+// })
+
+
+/////////////
+function googleMaps(queryCaptured) {
+    
+    ///////GOOGLE MAPS STUFF///////
+    // window.onload = function() {
+    var map;
+    var service;
+    var infowindow;
+    
+    function initMap() {
+
+        console.log("initMap Triggered")
+    
+        var queryToUse = "Grand Portage"
+
+        console.log(queryToUse)
+        var request = {
+        query: queryToUse,
+        fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
+        
+
+    }
+    service = new google.maps.places.PlacesService(document.createElement("div"));
+    service.findPlaceFromQuery(request, callback);
+    }
+
+    
+    function callback(results, status) {
+        console.log("callback Triggered")
+        console.log()
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+    
+            console.log(results[i]);
+        }
+        }
+        else{
+            console.log("NOPE THE MAPS THING DID NOT WORK")
+        }
+    }
+    initMap()
+    //closes the window on load function
+    // }
+
+}
+
+//Calling weather API
+var APIkey = "4f3188b24b25b236bcf5a2cffd75cfe6";
+var locationQuery = $("#cityInput").val().trim();
+var QueryURL ="api.openweathermap.org/data/2.5/forecast?q=" + locationQuery + "&units=imperial&appid=" + APIkey;
+
+$("#citySubmit").on("click", function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: QueryURL,
+        method: 'GET'
+    }).then(function(response) {
+        console.log(QueryURL);
+        console.log(response);   
+
+        $("#temp").text("Temperature: " + response.main.temp);
+        $("#wind").text("Wind Speed: " + response.wind.speed);
+        $("#humidity").text("Humidty: " + response.main.humidity);
+
+        console.log("Temperature: " + response.main.temp);
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidty: " + response.main.humidity);
     });
 });
